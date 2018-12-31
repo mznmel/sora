4ddc65cf567b6a510f0ccb9f3d5a444193b0b93f
Mazen A. Melibari <mazen@mazen.ws>
2018-12-31 15:10:05 +0300

    render images in a grid using pillow instead of html, because github doesn't support embedded css styles for jupyter notebooks.

--- sora/_sora.py
+++ sora/_sora.py
@@ -3,7 +3,9 @@ from IPython.display import HTML
 from glob import glob
 from os import path
 import base64
-
+from PIL import Image
+import io
+import math
 
 MIME_TYPES = {
     ".jpg": "image/jpeg",
@@ -18,32 +20,12 @@ def _image_to_base64_src(image, mime):
     return encoded_image_tmpl.format(mime, image_base64)
 
 
-def _html_img_item(src, item_width="33%", img_width="100%"):
+def _html_img(src):
     item_tmpl = """
-        <div style="
-            width: {1};
-            padding: 10px">
-            <img style="width: {2};" src="{0}"></img>
-        </div>
+        <img src="{0}"></img>
     """
 
-    return item_tmpl.format(src, item_width, img_width)
-
-
-def _html_simple_grid(html_images_items):
-    container_tmpl = """
-    <div style="
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        align-content: center;
-        flex-wrap: wrap">
-        {0}
-    </div>
-    """
-
-    html = container_tmpl.format("\n".join(html_images_items))
-    return html
+    return item_tmpl.format(src)
 
 
 def _html_display(html):
@@ -60,31 +42,65 @@ def _file_to_html(filepath, **kwargs):
 
     encoded_image = _image_to_base64_src(content, MIME_TYPES[extension])
 
-    return _html_img_item(encoded_image, **kwargs)
+    return _html_img(encoded_image, **kwargs)
 
-def sora(arg, **kwargs) :
 
-    # width percentage of each grid cell,
-    item_width = kwargs.get("width", "50%")
-    # width percentage of each image,
-    img_width = kwargs.get("width", "100%")
+def _images_to_grid(list_images, cell_width=32, cell_height=32, items_per_row=3):
+    # create a new canvas
 
-    html = ""
+    if len(list_images) <= items_per_row:
+        canvas_width = len(list_images) * cell_width
+    else:
+        canvas_width = items_per_row * cell_width
+    canvas_height = int(math.ceil(len(list_images) / items_per_row)) * cell_height
+    canvas = Image.new("RGB", (canvas_width, canvas_height), "white")
+
+    # draw the images in a grid
+    for i, img in enumerate(list_images):
+        x = (i % items_per_row) * cell_width
+        y = (int(math.floor(i/items_per_row))) * cell_height
+
+        img = list_images[i]
+        img.thumbnail((cell_width, cell_height))
+        canvas.paste(img, box=(x, y))
+        img.close()
+
+    with io.BytesIO() as output:
+        canvas.save(output, format="JPEG")
+        canvas_base64 = _image_to_base64_src(output.getvalue(), MIME_TYPES[".jpg"])
+
+    return _html_img(canvas_base64)
+
+
+def _load_directory(dir_path):
+    dir_full_path = path.abspath(dir_path)
+
+    all_imgs = []
+    for ext in MIME_TYPES:
+        files = glob(path.join(dir_full_path, "*{0}".format(ext)))
+        for f in files:
+            img = Image.open(f)
+            all_imgs.append(img)
+
+    return all_imgs
+
+
+def sora(arg, **kwargs):
+
     if isinstance(arg, str):
         if arg[-1] == "/":
             # arg is a string with a trailing slash,
             # let's find all the images and display them in a grid
-            dir_full_path = path.abspath(arg)
-            html_images_items = []
-            for ext in MIME_TYPES:
-                files = glob(path.join(dir_full_path, "*{0}".format(ext)))
-                html_images_items.extend([_file_to_html(f, **kwargs) for f in files])
-
-            html = _html_simple_grid(html_images_items)
+            all_images = _load_directory(arg)
+            cell_width = kwargs.get("cell_width", 32)
+            cell_height = kwargs.get("cell_height", 32)
+            items_per_row = kwargs.get("items_per_row", 2)
+            html = _images_to_grid(all_images, cell_width, cell_height, items_per_row)
+            return _html_display(html)
         else:
             # arg is a single file path
             html = _file_to_html(arg)
-    # return print(html)
+
     return display(HTML(html))
 
 
